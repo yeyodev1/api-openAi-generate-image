e974bb005bd2ae264255c1cf2bd27b3302632585
Diego Reyes <diegorele13@gmail.com>
2024-08-03 14:09:04 -0500

    format response to builderbot

--- src/controllers/openAi.controller.ts
+++ src/controllers/openAi.controller.ts
@@ -9,7 +9,17 @@ export async function generateImage (req: Request, res: Response) {
   try {
     const image = await ai.generateImage('Imagina que eres un diseñador gráfico y que un cliente te pide que diseñes unas tarjetas de presentación para un consultorio, debe ser un diseño elegante y moderno, el consultorio se llama odontoflex, realizas 2 propuestas,');
 
-    res.status(200).json({image});
+    const response = {
+      messages: [
+        {
+          type: 'to_user', 
+          media_url: image,
+          content: 'ejemplo',
+        }
+      ]
+    }
+
+    res.status(200).send(response);
   } catch (error) {
     console.error('errosss: ', error)
   }
