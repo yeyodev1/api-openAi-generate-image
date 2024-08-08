8be7d92ee859992dbeb06514d5da04e37ddaad3a
Diego Reyes <diegorele13@gmail.com>
2024-08-08 12:50:31 -0500

    add: history

--- src/controllers/openAi.controller.ts
+++ src/controllers/openAi.controller.ts
@@ -6,8 +6,17 @@ import AIClass from '../services/openai.class';
 const ai = new AIClass(process.env.OPEN_AI_KEY!);
 
 export async function generateImage (req: Request, res: Response) {
+
+  const { history } = req.body;
+  
+  console.log('esto es lo que llega', req.body)
   try {
-    const image = await ai.generateImage('Imagina que eres un diseñador gráfico y que un cliente te pide que diseñes unas tarjetas de presentación para un consultorio, debe ser un diseño elegante y moderno, el consultorio se llama odontoflex, realizas 2 propuestas,');
+
+
+    const prompt = 'Imagina que eres un diseñador gráfico y que un cliente te pide que diseñes unas tarjetas de presentación, esto siempre lo harás básandote en el historial que es el siguiente [{history}], debe ser un diseño elegante y moderno, el consultorio se llama odontoflex, realizas 2 propuestas'.replace('{history}', history);
+   
+   
+    const image = await ai.generateImage(prompt);
 
     const response = {
       messages: [
