939e978cab4b1d4855a84272dcc4027df42446a7
Diego Reyes <diegorele13@gmail.com>
2024-10-17 17:32:31 -0500

    cositas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,17 @@
 import express, { Application } from 'express';
 import { Server } from 'socket.io';
 
-import openAiRoutes from './openAi';
-import freepikRoutes from './freepik';
+// import openAiRoutes from './openAi';
+import client from './client'
 
 function routerApi(app: Application, io: Server) {
   const router = express.Router();
 
   app.use('/api', router);
 
-  router.use(openAiRoutes)
-  router.use(freepikRoutes)
+  // router.use(openAiRoutes)
+  router.use(client)
+
 }
 
 export default routerApi;
